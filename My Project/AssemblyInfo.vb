0b2377f77b85e69c63a0f3dea4ba1d1bd4693147
ogawa-kenji <ogawa.kenji.mail@gmail.com>
2019-10-22 11:54:18 +0900

    commit

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("UnitTestProject1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Hewlett-Packard Company")>
+<Assembly: AssemblyProduct("UnitTestProject1")>
+<Assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("f5911bba-220e-474a-bf10-6c359d1fecdd")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
