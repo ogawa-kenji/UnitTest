b00a4b100f7dea99d7d54c70fcc30945f56b7942
ogawa-kenji <ogawa.kenji.mail@gmail.com>
2019-10-22 12:25:03 +0900

    commit

--- UnitTest1.vb
+++ UnitTest1.vb
@@ -1,9 +1,38 @@
 ﻿Imports System.Text
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
-<TestClass()> Public Class UnitTest1
+<TestClass()> Public Class PersonTest
+    Private personName As String = "Bob"
+    Private personAge As Integer = 21
 
-    <TestMethod()> Public Sub TestMethod1()
+    <TestMethod()> Public Sub testCreatePersonWithNameAndAge()
+        Dim p = New Person(personName, personAge)
+        Assert.AreEqual(personName, p.getName())
+        Assert.AreEqual(personAge, p.getAge())
     End Sub
+
+End Class
+
+Friend Class Person
+    Private personName As String
+    Private personAge As Integer
+
+    Public Sub New(personName As String, personAge As Integer)
+        Me.personName = personName
+        Me.personAge = personAge
+    End Sub
+
+    Public ReadOnly Property getName() As String
+        Get
+            Return personName
+        End Get
+    End Property
+
+    Public ReadOnly Property getAge() As Integer
+        Get
+            Return personAge
+        End Get
+    End Property
+
 
 End Class
